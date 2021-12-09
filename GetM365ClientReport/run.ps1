29e077dc6f0c01c92b2688f7a55cda37da5dce53
majorwitteman <36670202+majorwitteman@users.noreply.github.com>
2021-12-09 15:36:40 -0500

    change to user list instead of report

--- GetM365ClientReport/run.ps1
+++ GetM365ClientReport/run.ps1
@@ -26,11 +26,10 @@ catch {
     Write-Error -Message "Could not get token using domain: $domain. Exception: $($_.Exception)" -ErrorAction Stop
 }
 
-# $reportUri = "https://graph.microsoft.com/v1.0/reports/getOffice365ActiveUserDetail(period='D30')"
-$userUri = 'https://graph.microsoft.com/v1.0/users?$filter=(accountEnabled eq true) and (userType eq ''Member'')&$select=userPrincipalName,displayName'
+$reportUri = "https://graph.microsoft.com/v1.0/reports/getOffice365ActiveUserDetail(period='D30')"
 
 $params = @{
-    Uri                     = $userUri
+    Uri                     = $reportUri
     Method                  = "Get"
     Headers                 = @{
         Authorization = "Bearer $($token.access_token)"
@@ -39,37 +38,17 @@ $params = @{
     StatusCodeVariable      = "resStatus"
 }
 
-$retryCount = 0
-$retryWait = 0
-$morePages = $true
-$userListResponse = do {
-    Start-Sleep -Milliseconds $retryWait
-    $userResponse = Invoke-RestMethod @params
-    if ($resStatus -ne 200) {
-        $retryWait = Get-Random -Minimum 100 -Maximum 2000
-        $retryCount += 1
-        continue
-    }
-    if ($userResponse."@odata.nextLink") {
-        $params.Uri = $userResponse."@odata.nextLink"
-    }
-    else {
-        $morePages = $false
-    }
-    $userResponse.value
-} until ($retryCount -gt 5 -or $morePages -eq $false)
-
-# try {
-#     $report = Invoke-RestMethod @params | ConvertFrom-Csv
-# }
-# catch {
-#     Write-Error "Report request failed for $($QueueItem.id) - $($QueueItem.identifier). Error $($_.Exception)" -ErrorAction Stop
-# }
+try {
+    $report = Invoke-RestMethod @params | ConvertFrom-Csv
+}
+catch {
+    Write-Error "Report request failed for $($QueueItem.id) - $($QueueItem.identifier). Error $($_.Exception)" -ErrorAction Stop
+}
 
 Write-Verbose "Report for $($QueueItem.id) - $($QueueItem.identifier) request status code: $resStatus"
-# $userList = $report | Select-Object -Property @{n = "DisplayName"; e = { $_."Display Name" } },
-# @{n = "samAccountName"; e = { $_."User Principal Name" } },
-# @{n = "lastLogonDate"; e = { Get-Date -Date (Get-UserLatestActivityDate -User $_) -Format "M/d/yyyy hh:mm:ss tt" } }
+$userList = $report | Select-Object -Property @{n = "DisplayName"; e = { $_."Display Name" } },
+@{n = "samAccountName"; e = { $_."User Principal Name" } },
+@{n = "lastLogonDate"; e = { Get-Date -Date (Get-UserLatestActivityDate -User $_) -Format "M/d/yyyy hh:mm:ss tt" } }
 
 $userList = $userListResponse |
 Select-Object -Property @{n = "samAccountName"; e = { $_.userPrincipalName } },
