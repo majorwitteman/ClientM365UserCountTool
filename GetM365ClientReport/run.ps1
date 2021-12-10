c2bab4225bcfded026261c909a24540f640e38f4
majorwitteman <36670202+majorwitteman@users.noreply.github.com>
2021-12-10 15:57:35 -0500

    report function change to email apps

--- GetM365ClientReport/run.ps1
+++ GetM365ClientReport/run.ps1
@@ -5,19 +5,7 @@ param($QueueItem, $TriggerMetadata)
 Write-Host "PowerShell queue trigger function processed work item: $($QueueItem.id) - $($QueueItem.identifier)"
 Write-Host "Queue item insertion time: $($TriggerMetadata.InsertionTime)"
 
-if ($QueueItem.invoiceToEmailAddress) {
-    $domain = ($QueueItem.invoiceToEmailAddress.Trim() -split '@')[1]
-    switch ($domain) {
-        "vizuriusa.com" { $domain = "propellatx.com" }
-        "wiley.com" { $domain = "jjeditorial.com" }
-    }
-    Write-Verbose "Using invoiceToEmailAddress: $domain"
-    
-}
-else {
-    $domain = Get-ContactDomain -ContactId $QueueItem.defaultContact.id
-    Write-Verbose "Using default contact $domain"
-}
+$domain = $QueueItem.defaultdomainname
 
 try {
     $token = Get-M365Token -Domain $domain
@@ -26,7 +14,7 @@ catch {
     Write-Error -Message "Could not get token using domain: $domain. Exception: $($_.Exception)" -ErrorAction Stop
 }
 
-$reportUri = "https://graph.microsoft.com/v1.0/reports/getOffice365ActiveUserDetail(period='D30')"
+$reportUri = "https://graph.microsoft.com/v1.0/reports/getEmailAppUsageAppsUserCounts(period='D30')"
 
 $params = @{
     Uri                     = $reportUri
@@ -42,24 +30,17 @@ try {
     $report = Invoke-RestMethod @params | ConvertFrom-Csv
 }
 catch {
-    Write-Error "Report request failed for $($QueueItem.id) - $($QueueItem.identifier). Error $($_.Exception)" -ErrorAction Stop
+    Write-Error "Report request failed for $($QueueItem.name) - $($domain). Error $($_.Exception)" -ErrorAction Stop
 }
 
-Write-Verbose "Report for $($QueueItem.id) - $($QueueItem.identifier) request status code: $resStatus"
-$userList = $report | Select-Object -Property @{n = "DisplayName"; e = { $_."Display Name" } },
-@{n = "samAccountName"; e = { $_."User Principal Name" } },
-@{n = "lastLogonDate"; e = { Get-Date -Date (Get-UserLatestActivityDate -User $_) -Format "M/d/yyyy hh:mm:ss tt" } }
+Write-Verbose "Report for $($QueueItem.name) - $($QueueItem.domain) request status code: $resStatus"
 
-$userList = $userListResponse |
-Select-Object -Property @{n = "samAccountName"; e = { $_.userPrincipalName } },
-displayName,
-@{n = "activeDate"; e = { (Get-Date).ToString(("M/d/yyyy hh:mm:ss tt")) } },
-@{n = "Source"; e = { "M365" } }
+$output = [pscustomobject]@{
+    company = $QueueItem.name
+    defaultdomain = $domain
+    pop3 = $report."pop3 app"
+    imap4 = $report."imap4 app"
+    smtp = $report."smtp app"
+} | ConvertTo-Csv | ForEach-Object { "$_`r`n" }
 
-$userListReport = [pscustomobject]@{
-    recId    = $QueueItem.id
-    company  = $QueueItem.identifier
-    userList = $userList
-}
-
-Push-OutputBinding -Name outBlob -Value $userListReport
+Push-OutputBinding -Name outBlob -Value ([System.Text.Encoding]::ASCII.GetBytes($output))
